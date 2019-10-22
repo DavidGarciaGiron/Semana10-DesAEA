539863583a075230d7d363e0ef79f20e2530cbb2
DavidGarciaGiron <david.garcia@tecsup.edu.pe>
2019-10-21 19:35:22 -0500

    first commit

---
+++ University/University/Models/OfficeAssignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
+{
+    public class OfficeAssignment
+    {
+        internal int active;
+
+        [Key]
+        [ForeignKey("Instructor")]
+
+        public int InstructorID { get; set; } 
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string location { get; set; }
+
+        public virtual Instructor Instructor { get; set; }
+
+    }
+}
---
+++ University/University/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
+{
+    public abstract class Person
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [StringLength(50, ErrorMessage = "El primer nombre no puede ser mayor a carateres.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+
+    }
+}
